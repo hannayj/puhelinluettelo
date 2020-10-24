d1e7786db304bc20136df9907a5a75ca291374c1
Hanna Ylä-Jussila <hanna.yla-jussila@myy.haaga-helia.fi>
2020-10-24 08:40:10 +0300

    puhelinluettelo with frontend

---
+++ build/precache-manifest.9a3c6585f57f88b2e4534839dc8714e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b32313903ec759b75cc1ca9f3c1b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "147a202a44d87096cfbf",
+    "url": "/static/css/main.58587478.chunk.css"
+  },
+  {
+    "revision": "1a15deb88bce7379561a",
+    "url": "/static/js/2.dbfacc78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dbfacc78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "147a202a44d87096cfbf",
+    "url": "/static/js/main.792313d5.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', function (require, response) {
     return JSON.stringify(require.body)
